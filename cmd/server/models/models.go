219c324b773bba2e83ae235056c3fc3a1b451702
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-04-06 16:57:41 +0900

    style(models): models를 사용 할 때 햇갈리지 않도록 주석 추가

--- cmd/server/models/models.go
+++ cmd/server/models/models.go
@@ -3,5 +3,6 @@ package models
 // GetModels returns the registered models
 func GetModels() []interface{} {
 	models := []interface{}{}
+	// models should contain the address of the target model's instance, like &User{}
 	return models
 }
