9e5332052b672ca23a3d371a9bc0c09d9626bb9a
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-04-06 16:56:11 +0900

    refactor(utils): db.go가 더 DRY 하도록 리팩토링

--- cmd/server/utils/db.go
+++ cmd/server/utils/db.go
@@ -18,6 +18,12 @@ type DBManager struct {
 	Instance *gorm.DB
 }
 
+func getDsn() string {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		configs.Envs["PSQL_HOST"], configs.Envs["PSQL_USER"], configs.Envs["PSQL_PASSWORD"], configs.Envs["PSQL_DBNAME"], configs.Envs["PSQL_PORT"], configs.Envs["PSQL_SSLMODE"], configs.Envs["PSQL_TIMEZONE"])
+	return dsn
+}
+
 // GetDB is a singleton constructor of the DBManager
 func GetDB() *DBManager {
 	once.Do(func() {
@@ -26,17 +32,28 @@ func GetDB() *DBManager {
 	return dbManager
 }
 
-// SetupDB setups the gorm DB configuration using the values from the environment
-func (m *DBManager) SetupDB() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
-		configs.Envs["PSQL_HOST"], configs.Envs["PSQL_USER"], configs.Envs["PSQL_PASSWORD"], configs.Envs["PSQL_DBNAME"], configs.Envs["PSQL_PORT"], configs.Envs["PSQL_SSLMODE"], configs.Envs["PSQL_TIMEZONE"])
+// OpenDB opens db
+func OpenDB(dsn string) *gorm.DB {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	for _, model := range models.GetModels() {
+	return db
+}
+
+// MigrateModels automatically migrates registered models
+func MigrateModels(db *gorm.DB, models []interface{}) {
+	for _, model := range models {
 		db.AutoMigrate(model)
 	}
+}
+
+// SetupDB setups the gorm DB configuration using the values from the environment
+func (m *DBManager) SetupDB() {
+	dsn := getDsn()
+	db := OpenDB(dsn)
+	models := models.GetModels()
+	MigrateModels(db, models)
 	m.Instance = db
 }
 
