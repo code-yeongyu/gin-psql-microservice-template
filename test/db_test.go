82f92787fdcbb7c30bbb799b6e299f4550f823b3
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-10 15:31:11 +0900

    test: db_test.go 개선
    - 테스트가 더 DRY하도록 개선: resetDB() 추가
    - DB 테스트용 util 테스트 추가: testClearDB() 추가
    - 테스트 db용 env를 사용하도록 변경

--- test/db_test.go
+++ test/db_test.go
@@ -2,27 +2,41 @@ package test
 
 import (
 	"fmt"
-	"gin_psql_microservice_template/cmd/server/models"
-	"gin_psql_microservice_template/cmd/server/utils"
 	"testing"
+	"user_service/cmd/server/models"
+	"user_service/cmd/server/utils"
+	"user_service/configs"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/tj/assert"
+	"gorm.io/gorm"
 )
+
+func resetDB(db *gorm.DB) {
+	for _, model := range models.GetModels() {
+		modelName := utils.GetTableName(model)
+		db.Exec(fmt.Sprintf("DELETE FROM %s", modelName)) // reset db data
+	}
+}
+
+func testClearDB(t *testing.T) {
+	db := utils.GetDB().Instance
+	resetDB(db)
+}
 
 func TestDBSingleton(t *testing.T) {
 	db1 := utils.GetDB()
 	db2 := utils.GetDB()
-	utils.GetDB().SetupDB()
+	utils.GetDB().SetupDB(models.GetModels())
 
 	assert.Equal(t, true, db1.Instance == db2.Instance)
 }
 
 func TestDBSetup(t *testing.T) {
+	configs.Envs["PSQL_DBNAME"] = "test_database"
+
 	db := utils.GetDB()
-	db.SetupDB()
-	for _, model := range models.GetModels() {
-		modelName := utils.GetType(model)
-		db.Instance.Exec(fmt.Sprintf("DELETE FROM %s", modelName)) // reset db data
-	}
+	db.SetupDB(models.GetModels())
+
+	resetDB(db.Instance)
 }
